f64d080c4df8fc14875375b028f083b8b5533bd2
Vladislav Forsh <vlad@robowhale.com>
2026-06-13 09:12:16 +0300

    chore: release 0.3.0

--- src/cli/program.ts
+++ src/cli/program.ts
@@ -20,7 +20,7 @@ export function buildProgram(): Command {
 	program
 		.name("bwx")
 		.description("Bitwarden Extended CLI")
-		.version("0.2.2")
+		.version("0.3.0")
 		.option("--json", "JSON output")
 		.option("--plain", "Plain parseable output")
 		.option("-q, --quiet", "Suppress log output")
